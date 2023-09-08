26d073625d3ed2b0e9715a4fbf5228824bdf7074
naga <toritonsuibu0527@gmail.com>
2023-09-08 18:21:05 +0900

    モータードライバ周りのライブラリの追加

---
+++ inc/MD.hpp
@@ -0,0 +1,63 @@
+#pragma once
+
+#include<cmath>
+#include<stdint.h>
+
+#ifdef STM32_NAGA_LIB
+#include<main.h>
+#endif //STM32_NAGA_LIB
+
+namespace naga_libs{
+class MD {
+public:
+	virtual void operator () (int32_t) = 0;
+
+	virtual void set_reverse(bool reverse_ = false) noexcept = 0;
+};
+
+#ifdef ARDUINO_NAGA_LIB
+class MD_Arduino{
+	const uint8_t PIN_A, PIN_B, pwm_pin;
+	int pwm;
+public:
+	MD_Arduino(uint8_t _PIN_A, uint8_t _PIN_B, uint8_t _pwm_pin):PIN_A(_PIN_A), PIN_B(_PIN_B), pwm_pin(_pwm_pin){}
+	~MD_Arduino() = default;
+	void operator () (int re);
+};
+#endif //ARDUINO_NAGA_LIB
+
+#ifdef STM32_NAGA_LIB
+struct quad_MD_info 
+{
+	GPIO_TypeDef* port_A; uint16_t PIN_A;
+	GPIO_TypeDef* port_B; uint16_t PIN_B;
+
+	TIM_HandleTypeDef* pwm_port;
+	uint32_t pwm_channel;
+
+	quad_MD_info(quad_MD_info&&);
+	quad_MD_info(GPIO_TypeDef* _port_A, uint16_t _PIN_A, GPIO_TypeDef* _port_B, uint16_t _PIN_B, TIM_HandleTypeDef* _pwm_port, uint32_t _pwm_channel);
+	quad_MD_info() = delete;
+
+};
+
+class quad_MD : public MD 
+{
+	bool is_reverse;
+	int32_t pwm;
+
+	quad_MD_info info;
+
+public:
+	explicit quad_MD(GPIO_TypeDef*, uint16_t, GPIO_TypeDef*, uint16_t, TIM_HandleTypeDef *, uint32_t);
+	explicit quad_MD(quad_MD_info&);
+	explicit quad_MD(quad_MD_info&&);
+
+	void operator ()(int32_t) override;
+	void set_reverse(bool reverse_ = false) noexcept override;
+
+	quad_MD() = delete;
+
+};
+#endif //STM32_NAGA_LIB
+};
---
+++ src/stm32MD.cpp
@@ -0,0 +1,55 @@
+#define STM32_NAGA_LIB
+
+#include "MD.hpp"
+
+using namespace naga_libs;
+
+quad_MD_info::quad_MD_info(quad_MD_info& cp) 
+: port_A(cp.port_A), PIN_A(cp.PIN_A), 
+  port_B(cp.port_B), PIN_B(cp.PIN_B),
+  pwm_port(cp.pwm_port), pwm_channel(cp.pwm_channel) 
+{
+}
+
+quad_MD_info::quad_MD_info(quad_MD_info&& cp) 
+: port_A(cp.port_A), PIN_A(cp.PIN_A), 
+  port_B(cp.port_B), PIN_B(cp.PIN_B),
+  pwm_port(cp.pwm_port), pwm_channel(cp.pwm_channel) 
+{
+}
+
+quad_MD_info::quad_MD_info(GPIO_TypeDef* _port_A, uint16_t _PIN_A, GPIO_TypeDef* _port_B, uint16_t _PIN_B, TIM_HandleTypeDef* _pwm_port, uint32_t _pwm_channel)
+: port_A(_port_A),PIN_A(_PIN_A), port_B(_port_B),PIN_B(_PIN_B), pwm_port(_pwm_port), pwm_channel(_pwm_channel) 
+{
+}
+
+explicit quad_MD::quad_MD(GPIO_TypeDef* port_A, uint16_t PIN_A, GPIO_TypeDef* port_B, uint16_t PIN_B, TIM_HandleTypeDef *pwm_port, uint32_t pwm_channel) 
+: info(port_A, PIN_A, port_B, PIN_B, pwm_port, pwm_channel)
+{
+	HAL_TIM_PWM_Start(info.pwm_port, info.pwm_channel);
+}
+
+explicit quad_MD::quad_MD(quad_MD_info& _info)
+: info(_info)
+{
+    HAL_TIM_PWM_Start(info.pwm_port, info.pwm_channel);
+}
+
+explicit quad_MD::quad_MD(quad_MD_info&& _info)
+: info(_info)
+{
+    HAL_TIM_PWM_Start(info.pwm_port, info.pwm_channel);
+}
+
+void quad_MD::operator () (int32_t _pwm) override
+{
+    pwm = _pwm * ((not is_reverse) ? 1 : -1);
+    HAL_GPIO_WritePin(port_A, PIN_A, pwm >= 0 ? GPIO_PIN_SET : GPIO_PIN_RESET);
+    HAL_GPIO_WritePin(port_B, PIN_B, pwm <  0 ? GPIO_PIN_SET : GPIO_PIN_RESET);
+    __HAL_TIM_SET_COMPARE(pwm_port, pwm_channel, std::abs(pwm));
+}
+
+void quad_MD::set_reverse(bool reverse = false) noexcept override
+{
+    is_reverse = reverse;
+}
